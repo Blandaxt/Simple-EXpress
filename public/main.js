a723461e92ce0c7976020bce11777de9698a2526
resilient <resilient@resilients-MacBook-Pro.local>
2018-07-03 17:07:42 -0400

    initial commit

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
@@ -6,6 +7,7 @@ Array.from(thumbUp).forEach(function(element) {
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        // const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
         fetch('messages', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
@@ -13,6 +15,33 @@ Array.from(thumbUp).forEach(function(element) {
             'name': name,
             'msg': msg,
             'thumbUp':thumbUp
+            // 'thumbDown': thumbDown
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        // const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+        fetch('messages2', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            // 'thumbUp':thumbUp
+            'thumbDown': thumbDown
           })
         })
         .then(response => {
